9a4a1406b03457524d8c62816d99dbc79fe06aac
Michele Gallo <michelegallo997@gmail.com>
2024-10-08 16:28:08 -0300

    simulador_inicial_Michele_Gallo1

--- app.js
+++ app.js
@@ -1,22 +1,19 @@
-// let meta= parseFloat(prompt("Cual es tu meta financiera en dolares?"));
-// let tiempo= parseInt(prompt("en cuantos meses quieres lograrlo?"));
-// let ahorroInicial= parseFloat(prompt("¿Cuánto tienes ahorrado actualmente? (en dólares)"));
-// if(isNaN(meta) || isNaN(tiempo) || isNaN(ahorroInicial) || meta<=0 || tiempo<=0 || ahorroInicial<=0){
-//     alert("por favor, ingrese valores validos ")
-// }
+let meta= parseFloat(prompt("Cual es tu meta financiera en dolares?"));
+let tiempo= parseInt(prompt("en cuantos meses quieres lograrlo?"));
+let ahorroInicial= parseFloat(prompt("¿Cuánto tienes ahorrado actualmente? (en dólares)"));
+if(isNaN(meta) || isNaN(tiempo) || isNaN(ahorroInicial) || meta<=0 || tiempo<=0 || ahorroInicial<=0){
+    alert("por favor, ingrese valores validos ")
+}
 
-// let ahorroMensual= (meta - ahorroInicial)/tiempo;
+let ahorroMensual= (meta - ahorroInicial)/tiempo;
 
-// let mensaje= "debes ahorrar" +ahorroMensual.toFixed(2)+ "cada mes para alcazar tu meta de "+ meta + "dolares";
+let mensaje= "debes ahorrar" +ahorroMensual.toFixed(2)+ "cada mes para alcazar tu meta de "+ meta + "dolares";
 
-// let ahorroTotal = ahorroInicial;
-// for (let mes = 1; mes <= tiempo; mes++) {
-//     ahorroTotal += ahorroMensual;
-//     mensaje += `\nEn el mes ${mes}, habrás ahorrado $${ahorroTotal.toFixed(2)}.`;
-// }
-// alert(mensaje);
+let ahorroTotal = ahorroInicial;
+for (let mes = 1; mes <= tiempo; mes++) {
+    ahorroTotal += ahorroMensual;
+    mensaje += `\nEn el mes ${mes}, habrás ahorrado $${ahorroTotal.toFixed(2)}.`;
+}
+alert(mensaje);
 
 
-let array= [0, 1, 2, 4];
-
-console.log(array[-1]);
--- js_curso.js
+++ js_curso.js
@@ -1,22 +1,42 @@
 // Objetos en javascripts
 // ejemplo persona
-let nombre="luffy";
-let edad= 19;
-let  anime="One piece"
-let persona={
-    nombre:"luffy",
-    edad: 19,
-    anime:"One piece",
-};
-console.log(persona);
-console.log(persona.nombre);
-console.log(persona["edad"]);
 
 
-delete persona.edad;
-console.log(persona);
+// let nombre="luffy";
+// let edad= 19;
+// let  anime="One piece"
+// let persona={
+//     nombre:"luffy",
+//     edad: 19,
+//     anime:"One piece",
+// }
+// console.log(persona);
+// console.log(persona.nombre);
+// console.log(persona["edad"]);
+// delete persona.edad;
+// console.log(persona);
 
-// Objetos en Array
-// ejemplo animale
-let animales=["pato","gato", "perro", "conejo", "camello"];
-console.log(animales);
+
+
+// // Objetos en Array
+// // ejemplo animale
+
+// let animales=["pato","gato", "perro", "conejo", "camello"];
+// console.log(animales[0]);
+// animales[2]="dragon";
+// console.log(animales);
+// console.log(animales.length);
+
+// FUNTION EN JAVASCRIPT
+function saludar(){
+    console.log("hola mundo");
+    
+}
+saludar();
+
+function suma(){
+    return 2 + 2;
+}
+
+let resultado=suma();
+console.log(resultado)
