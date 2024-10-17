a3d407f83d4c2c8c7aabd8ac3d6396b03e4911d1
Michele Gallo <michelegallo997@gmail.com>
2024-10-17 17:35:05 -0300

    simulador_inicial_Michele_Gallo8

--- ejercicos.js
+++ ejercicos.js
@@ -164,12 +164,64 @@
 // console.log(uperPalabra);
 
 
-function arrayTranformacion(array, transformacion){
-   return array.map(transformacion);
-}
-let numeros=[4, 5, 6, 4, 12, 19, -8];
-let nombre=["michele", "juan", "vannesa", "eden", "libia"];
+// function arrayTranformacion(array, transformacion){
+//    return array.map(transformacion);
+// }
+// let numeros=[4, 5, 6, 4, 12, 19, -8];
+// let nombre=["michele", "juan", "vannesa", "eden", "libia"];
 
-let nombreUper= arrayTranformacion(nombre, number => number.toUpperCase());
+// let nombreUper= arrayTranformacion(nombre, num => num.toUpperCase());
+// let numberes= arrayTranformacion(numeros, pares.filter(pares % 2 === 0) );
+// console.log(nombreUper);
+// console.log(numberes);
 
-console.log(nombreUper);
+
+// let frutas= ["pera", "manzana", "pera", "banana", "cereza", "banana", "pera"];
+// let conteoFrutas= frutas.reduce((conteo, fruta)=>{
+//    conteo[fruta]= (conteo[fruta] || 0) +1;
+//    return conteo;
+// }, {} );
+
+// console.log(conteoFrutas);
+
+
+const palabras = ['manzana', 'perro', 'elefante', 'gato'];
+// const palabraFilter= palabras.filter(palabras=> palabras.length>=5);
+// console.log(palabraFilter);
+
+
+// const reducePalabra= palabras.reduce((conteo, palabra)=>{
+//    if (palabra.length<=5){
+//       return conteo +1;
+//    }
+//    return conteo
+// }, 0);
+// console.log(reducePalabra);
+
+
+let combinarArray= palabras.reduce((acomulador, palabra)=> acomulador + " " + palabra);
+console.log(combinarArray);
+
+
+let contarLetras=  palabras.reduce((acomulador, palabras)=> (acomulador + palabras.length),0);
+console.log(contarLetras);
+
+
+const numeros = [1, 2, 2, 3, 4, 4, 4, 5];
+
+const contarNumeros= numeros.reduce((acomulador, number)=>{
+   acomulador[number]= (acomulador[number] || 0) +1;
+   return acomulador
+}, {});
+console.log(contarNumeros);
+
+
+const personas = [
+   { nombre: 'Juan', edad: 25 },
+   { nombre: 'María', edad: 30 },
+   { nombre: 'Pedro', edad: 35 }
+];
+
+
+const nombrePersona= personas.map(person => person["nombre"]);
+console.log(nombrePersona);
